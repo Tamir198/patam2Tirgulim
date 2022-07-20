f9f39ecc74854845913543b030ee7edd6091031e
Tamir Abutbul <1tamir198@gmail.com>
2022-07-20 16:56:49 +0300

    Add CompletableFuture example

--- src/Thread_pools_and_Futures/ThreadPoolExample.java
+++ src/Thread_pools_and_Futures/ThreadPoolExample.java
@@ -3,6 +3,7 @@ package Thread_pools_and_Futures;
 import org.omg.Messaging.SyncScopeHelper;
 
 import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 public class ThreadPoolExample {
 
@@ -18,7 +19,8 @@ public class ThreadPoolExample {
             System.out.println("Task started" + id);
             try {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {}
+            } catch (InterruptedException e) {
+            }
 
             System.out.println("The task is finished" + id);
         }
@@ -26,46 +28,73 @@ public class ThreadPoolExample {
 
     //Callable - A task that returns a result and may throw an exception.
     // Implementors define a single method with no arguments called call.
-    public static class ExampleTask2 implements Callable<Integer>{
+    public static class ExampleTask2 implements Callable<Integer>, Supplier<Integer> {
 
         //Callable method - need to return Integer (ar any <V> type)
         @Override
-        public Integer call(){
+        public Integer call() {
             //mimic "calculation" for 2 seconds
             try {
                 Thread.sleep(2000);
-            } catch (InterruptedException e) {}
+            } catch (InterruptedException e) {
+            }
             //After calculator ended return
             return 42;
+        }
+
+        //Supplier method - this won't throw exception
+        @Override
+        public Integer get() {
+            return 45;
         }
     }
 
     public static void main(String[] args) {
         //To create thread pool we need some executor service
-        ExecutorService es = Executors.newFixedThreadPool(3 );
+        ExecutorService es = Executors.newFixedThreadPool(3);
 
-        /***Use execute for void values and no calculations method
-        Use submit for Callables - a task with calculations and resault **/
         es.execute(new ExampleTask(1));
         es.execute(new ExampleTask(2));
         es.execute(new ExampleTask(3));
-        es.execute(new ExampleTask(4));
-        es.execute(new ExampleTask(5));
 
-        //Just like js async submit returns promise
-        Future<Integer> myPromise =  es.submit(new ExampleTask2());
-        try {
-            //This is like await in js
-            Integer i = myPromise.get();
-            System.out.println(Thread.currentThread().getName() + "return Result from promise = " + i);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {}
+        //Receive supplier, return CompletableFuture - we can run something in async
+        //ThenApply == then in js
+        //We will use the accept after we are done with all the "then"
+        CompletableFuture.supplyAsync(new ExampleTask2(), es)
+                .thenApply(res -> res * 2)
+                .thenApply(res -> "The value is " + res)
+                .thenAccept(res -> System.out.println(res));
 
-
-        //Wait for all tasks to be finished and only then c;close es
         es.shutdown();
         System.out.println("Main is death now");
+
+
+        //THIS EXAMPLE IS USING FUTURE, MAKES THE MAIN THREAD WAIT
+        //UNTIL SOMETHING WILL END, THIS IS WASTE OR RESOURCES
+        //LOOK UP FOR COMPLETABLE FUTURE SOLUTION - A BETTER ONE.
+//
+//        /***Use execute for void values and no calculations method
+//        Use submit for Callables - a task with calculations and resault **/
+//        es.execute(new ExampleTask(1));
+//        es.execute(new ExampleTask(2));
+//        es.execute(new ExampleTask(3));
+//        es.execute(new ExampleTask(4));
+//        es.execute(new ExampleTask(5));
+//
+//        //Just like js async submit returns promise
+//        Future<Integer> myPromise =  es.submit(new ExampleTask2());
+//        try {
+//            //This is like await in js
+//            Integer i = myPromise.get();
+//            System.out.println(Thread.currentThread().getName() + "return Result from promise =     " + i);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (ExecutionException e) {}
+//
+//
+//        //Wait for all tasks to be finished and only then c;close es
+//        es.shutdown();
+//        System.out.println("Main is death now");
     }
 
 }
