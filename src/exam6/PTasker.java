fea5aa3df08c1c4f69e8cfad32d2f3cbf57f1602
Tamir Abutbul <1tamir198@gmail.com>
2022-07-24 20:25:09 +0300

    SUBMIT ACCEPT A CALLABLE AND NOT THREAD

--- src/exam6/PTasker.java
+++ src/exam6/PTasker.java
@@ -1,17 +1,31 @@
-//package test;
-//
-//import java.util.List;
-//import java.util.concurrent.Future;
-//import java.util.function.BinaryOperator;
-//
-//public class PTasker {
-//
-//
-//	public  <V> Future<V> apply(List<V> buff, V identity, BinaryOperator<V> bo){
-//		return null;
-//	}
-//
-//	public void close(){
-//
-//	}
-//}
+package exam6;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.BinaryOperator;
+
+public class PTasker {
+    ExecutorService es;
+
+    public PTasker() {
+        es = Executors.newSingleThreadExecutor();
+    }
+
+    public <V> Future<V> apply(List<V> buff, V identity, BinaryOperator<V> bo) {
+        //return es.submit(()->buff.stream().reduce(identity,bo));
+       return es.submit(() ->{
+            V result = identity;
+            for (V element : buff) {
+                result = bo.apply(result, element);
+            }
+            return result;
+        });
+    }
+
+    public void close() {
+        es.shutdown();
+    }
+}
--- src/exam6/Singleton.java
+++ src/exam6/Singleton.java
@@ -1,15 +1,14 @@
-//package test;
-//
-//
-//public class Singleton {
-//
-//
-//
-//	public static <V> V getInstance(Class<V> c) throws Exception{
-//		// recall:
-//		// c.newInstance() creates a new instance of type VMID
-//		// c.getName() returns a String - the name of type V
-//
-//		return null;
-//	}
-//}
+package exam6;
+
+
+public class Singleton {
+
+
+	public static <V> V getInstance(Class<V> c) throws Exception{
+		// recall:
+		// c.newInstance() creates a new instance of type VMID
+		// c.getName() returns a String - the name of type V
+
+		return null;
+	}
+}
